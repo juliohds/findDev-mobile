d2bf8e86ec76083baf64b4ba0a7d8f98f6e1aa19
Julio Henrique <juliohenrique@Julios-MacBook-Air.local>
2020-01-19 21:32:59 -0300

    finish app fase 1 pages and location

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker, Callout } from 'react-native-maps';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TouchableOpacity, TextInput } from 'react-native';
 
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
+import api from './../services/api';
 
-export default function Main() {
+export default function Main({ navigation }) {
+
+  const [devs, setDevs] = useState([]);
+  const [techs, setTechs] = useState('');
   const [currentRegion, setCurrentRegion] = useState(null);
 
   useEffect(() => {
@@ -15,8 +20,8 @@ export default function Main() {
         const { coords } = await getCurrentPositionAsync({
           enableHighAccuracy: true,
         });
-
         const { latitude, longitude } = coords;
+        console.log("longitude", longitude)
 
         setCurrentRegion({
           latitude,
@@ -30,25 +35,62 @@ export default function Main() {
     loadInitialLocation();
   }, []);
 
+  async function loadDevs(){
+    const { latitude, longitude } = currentRegion;
+    const response = await api.get('/search', {
+      params: {
+        latitude,
+        longitude,
+        techs
+      }
+    });
+
+    console.log("dev", response.data)
+
+    setDevs(response.data.devs);
+  }
+
+  function handleRegionChanged(region){
+    setCurrentRegion(region);
+  }
+
   if(!currentRegion){
     return null;
   }
 
   return (
-    <MapView initialRegion={currentRegion} style={styles.map} >
-      <Marker coordinate={{ latitude: -27.211111164, longitude: -49.6374491 }}>
-      <Image soure={{ uri: ''}} style={{styles.avatar}} />
-      <Callout onPress={() => {
-
-      }}>
-        <View styles={{styles.callout}}>
-          <Text styles={{styles.devName}}>Julio Henrique</Text>
-          <Text styles={{styles.devBio}}>CTO</Text>
-          <Text styles={{styles.callout}}>ReactJS, ReactNative, Node.js</Text>
-        </View>
-      </Callout>
-      </Marker>
+    <>
+    <MapView onRegionChangeComplete={handleRegionChanged} initialRegion={currentRegion} style={styles.map} >
+      {devs.map(dev => (
+        <Marker key={dev._id} coordinate={{ latitude: dev.location.coordinates[1], longitude: dev.location.coordinates[0] }}>
+        <Image source={{uri: dev.avatar_url}} style={styles.avatar} />
+        <Callout onPress={() => {
+            navigation.navigate('Profile', { github_username: dev.github_username});
+        }}>
+          <View style={styles.callout}>
+            <Text style={styles.devName}>{dev.name}</Text>
+            <Text style={styles.devBio}>{dev.bio}</Text>
+            <Text style={styles.callout}>{dev.techs.join(', ')}</Text>
+          </View>
+        </Callout>
+        </Marker>
+      ))}
     </MapView>
+    <View style={styles.searchForm}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Buscar devs por techs..."
+        placeholderTextColor="#999"
+        autoCapitalize="words"
+        autoCorrect={false}
+        value={techs}
+        onChangeText={setTechs}
+      />
+      <TouchableOpacity style={styles.loadButton} onPress={loadDevs}>
+        <MaterialIcons name="my-location" size={20} color="#FFF" />
+      </TouchableOpacity>
+    </View>
+    </>
   );
 }
 
@@ -76,6 +118,41 @@ const styles = StyleSheet.create({
   },
   devTechs: {
     marginTop: 5,
+  },
+  searchForm: {
+    position: 'absolute',
+    top: 20,
+    left: 20,
+    right: 20,
+    zIndex: 5,
+    flexDirection: 'row'
+  },
+
+  searchInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#FFF',
+    color: '#333',
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.2,
+    textShadowOffset: {
+      width: 4,
+      height: 4,
+    },
+    elevation: 2
+  },
+
+  loadButton: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#8E4Dff',
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 15,
   }
 
 })
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-// import { Container } from './styles';
+export default function Profile({ navigation }) {
 
-export default function Pages() {
+  const githubUsername = navigation.getParam('github_username');
+
   return (
-    <View />
+    <WebView style={{flex: 1}} source={{ uri: `https://github.com/${githubUsername}`}} />
   );
 }
